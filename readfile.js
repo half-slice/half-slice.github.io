dc7fa515d6b244dbc4bcb8402c306e7db4401e1c
halfslice <junbin2438@naver.com>
2021-03-28 05:51:20 +0000

    length testing

--- readfile.js
+++ readfile.js
@@ -1,12 +1,12 @@
 function loadFile(filePath) {
-  var result = null;
-  var xmlhttp = new XMLHttpRequest();
-  xmlhttp.open("GET", filePath, false);
-  xmlhttp.send();
-  if (xmlhttp.status==200) {
-    result = xmlhttp.responseText;
-  }
-	
-	
-  alert(typeof(result));
+	var result = null;
+ 	var xmlhttp = new XMLHttpRequest();
+ 	xmlhttp.open("GET", filePath, false);
+ 	xmlhttp.send();
+ 	if (xmlhttp.status==200) {
+		result = xmlhttp.responseText;
+	}
+
+	alert(result.length);
+	return result.split("\n");  //배열로 리턴
 }
